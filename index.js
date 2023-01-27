a252b1a07099efe6e67f7ce28487af88245a6c9e
souravbaidya21 <33472784+souravbaidya21@users.noreply.github.com>
2023-01-27 12:54:48 +0530

    Dice value updated to image source

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var dice1=Math.floor((Math.random()*6)+1);
-var diceImage1= "images/dice" + dice1 + ".png";
+var diceImage1= "dice" + dice1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src",diceImage1);
 
 var dice2=Math.floor((Math.random()*6)+1);
-var diceImage2= "images/dice" + dice2 + ".png";
+var diceImage2= "dice" + dice2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src",diceImage2);
 
     if (dice1===dice2){
