dbc03a920d7b24bc756de0fedafafb0c101bba8b
souravbaidya21 <33472784+souravbaidya21@users.noreply.github.com>
2023-01-27 12:46:36 +0530

    Dice Game Complete

---
+++ index.js
@@ -0,0 +1,15 @@
+var dice1=Math.floor((Math.random()*6)+1);
+var diceImage1= "images/dice" + dice1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src",diceImage1);
+
+var dice2=Math.floor((Math.random()*6)+1);
+var diceImage2= "images/dice" + dice2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src",diceImage2);
+
+    if (dice1===dice2){
+        document.querySelector("h1").innerHTML="Draw!";
+    }
+    
+    if (dice1>dice2){document.querySelector("h1").innerHTML="🚩Player 1 Wins!";}
+    if (dice1<dice2){document.querySelector("h1").innerHTML="Player 2 Wins!🚩";}
+    
